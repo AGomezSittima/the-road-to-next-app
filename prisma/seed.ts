f7ab22a1b59d9ba9615812cabdbedd0581070bbc
AGomezSittima <agomezsittimadeveloper@gmail.com>
2025-02-27 01:09:11 +0100

    .env.template
    prisma
    
    database seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const tickets = [
+  {
+    title: "Ticket 1",
+    content: "This is the first ticket from the database.",
+    status: "DONE" as const,
+  },
+  {
+    title: "Ticket 2",
+    content: "This is the second ticket from the database.",
+    status: "OPEN" as const,
+  },
+  {
+    title: "Ticket 3",
+    content: "This is the third ticket from the database.",
+    status: "IN_PROGRESS" as const,
+  },
+  {
+    title: "Lorem ipsum database",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis malesuada sem, non varius turpis malesuada auctor. Nam mollis sem vitae sagittis pretium. Nullam id aliquam orci. Sed varius molestie viverra. Vestibulum porta ut felis porta dignissim. Phasellus consectetur nisl vitae fermentum convallis. Praesent ultricies scelerisque ex, eget hendrerit ex tincidunt eget. Aliquam erat volutpat. Aliquam ornare, lacus eget rutrum ornare, est sapien rutrum ex, et eleifend magna sem ut leo. Quisque posuere dolor vitae arcu facilisis varius. Quisque sed mollis mauris, sit amet molestie ex. Nam tincidunt mi vel ante pretium vestibulum. Praesent eu vehicula dolor. Proin ultrices lectus non neque dapibus, rutrum volutpat mauris placerat. Cras sed risus mi. Sed magna velit, condimentum vitae varius nec, dapibus sed.",
+    status: "OPEN" as const,
+  },
+];
+
+const seed = async () => {
+  const t0 = performance.now();
+  console.log("DB Seed: Started ...");
+
+  await prisma.ticket.deleteMany();
+
+  await prisma.ticket.createMany({
+    data: tickets,
+  });
+
+  const t1 = performance.now();
+  console.log(`DB Seed: Finished (${t1 - t0}ms) ...`);
+};
+
+seed();

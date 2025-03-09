b34c72045cfc617948ea43fb69322a0151fd1b7a
AGomezSittima <agomezsittimadeveloper@gmail.com>
2025-03-09 17:30:46 +0100

    add lucia libraries
    auth paths
    
    sign up and sign in header links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,44 @@
 import { LucideKanban } from "lucide-react";
 import Link from "next/link";
 
-import { homePath, ticketsPath } from "@/utils/path";
+import { homePath, signInPath, signUpPath, ticketsPath } from "@/utils/path";
 
 import { ThemeSwitcher } from "./theme/theme-switcher";
 import { buttonVariants } from "./ui/button";
 
+type NavItem = {
+  path: string;
+  name: string;
+  buttonVariant?:
+    | "default"
+    | "outline"
+    | "destructive"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | null
+    | undefined;
+};
+
 const Header = () => {
+  const navItems: NavItem[] = [
+    {
+      path: ticketsPath(),
+      name: "Tickets",
+      buttonVariant: "default",
+    },
+    {
+      path: signUpPath(),
+      name: "Sign Up",
+      buttonVariant: "outline",
+    },
+    {
+      path: signInPath(),
+      name: "Sign In",
+      buttonVariant: "outline",
+    },
+  ];
+
   return (
     <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b bg-background/95 px-5 py-2.5 backdrop-blur">
       <div className="align-items flex gap-x-2">
@@ -20,12 +52,15 @@ const Header = () => {
       </div>
       <div className="align-items flex gap-x-2">
         <ThemeSwitcher />
-        <Link
-          href={ticketsPath()}
-          className={buttonVariants({ variant: "default" })}
-        >
-          Tickets
-        </Link>
+        {navItems.map((navItem) => (
+          <Link
+            key={navItem.path}
+            href={navItem.path}
+            className={buttonVariants({ variant: navItem.buttonVariant })}
+          >
+            {navItem.name}
+          </Link>
+        ))}
       </div>
     </nav>
   );

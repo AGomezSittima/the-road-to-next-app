a65c487adef13647e1c8f6868ccdda361be0d192
AGomezSittima <agomezsittimadeveloper@gmail.com>
2025-03-26 18:52:41 +0100

    email template polish

--- src/emails/password/email-password-reset.tsx
+++ src/emails/password/email-password-reset.tsx
@@ -1,9 +1,13 @@
 import {
   Body,
   Button,
+  Column,
   Container,
   Head,
+  Heading,
   Html,
+  Preview,
+  Row,
   Section,
   Tailwind,
   Text,
@@ -19,21 +23,42 @@ const EmailPasswordReset = ({ toName, url }: EmailPasswordResetProps) => {
     <Html>
       <Head />
       <Tailwind>
-        <Body className="m-8 text-center font-sans">
+        <Body className="m-8 font-sans">
+          <Preview>TicketBounty reset your password</Preview>
+          <Heading className="text-center text-xl font-bold">
+            TicketBounty
+          </Heading>
+          <Section>
+            <Row>
+              <Column className="h-[2px] w-1/3 rounded-l bg-black/10" />
+              <Column className="h-[2px] w-1/3 rounded bg-purple-700" />
+              <Column className="h-[2px] w-1/3 rounded-r bg-black/10" />
+            </Row>
+          </Section>
           <Container>
             <Section>
               <Text>
-                Hello {toName}, you have requested to reset your password. Click
-                the button below to reset your password.
+                Hello <strong className="font-bold">{toName}</strong>,
+              </Text>
+              <Text>
+                Someone recently requested a password change for your
+                TicketBounty account. If it was you, you can set a new password
+                here:
               </Text>
             </Section>
-            <Section>
+            <Section className="text-center">
               <Button
                 href={url}
-                className="m-2 rounded bg-black p-2 text-white"
+                className="m-2 self-center rounded bg-purple-700 p-2 text-center text-white"
               >
                 Reset Password
               </Button>
+            </Section>
+            <Section>
+              <Text>
+                If you don&apos;t want to change your password or didn&apos;t
+                request this, just ignore and delete this message.
+              </Text>
             </Section>
           </Container>
         </Body>

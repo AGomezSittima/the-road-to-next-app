f5eb303493cf703fa7862e7c904974062bb71109
AGomezSittima <agomezsittimadeveloper@gmail.com>
2025-05-13 16:05:31 +0200

    admin area badge

--- src/app/(authenticated)/organization/[organizationId]/(admin)/layout.tsx
+++ src/app/(authenticated)/organization/[organizationId]/(admin)/layout.tsx
@@ -1,3 +1,4 @@
+import { LucideShieldUser } from "lucide-react";
 import React from "react";
 
 import { getAdminOrRedirect } from "@/features/membership/queries/get-admin-or-redirect";
@@ -15,5 +16,15 @@ export default async function AuthenticatedLayout({
 
   await getAdminOrRedirect(organizationId);
 
-  return <>{children}</>;
+  return (
+    <>
+      <div className="flex flex-col gap-y-4">
+        <div className="flex items-center gap-x-2">
+          <LucideShieldUser className="size-8" />
+          <span className="text-muted-foreground">You are in the admin area.</span>
+        </div>
+        {children}
+      </div>
+    </>
+  );
 }

972c783dbb95b4f94ea9bb5b65062c539c863951
AGomezSittima <agomezsittimadeveloper@gmail.com>
2025-08-27 23:52:31 +0200

    delete dangling attachments on organization deletion

--- src/features/organization/actions/delete-organization.ts
+++ src/features/organization/actions/delete-organization.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { generateS3Key } from "@/features/attachments/utils/generate-s3-key";
 import { getAdminOrRedirect } from "@/features/membership/queries/get-admin-or-redirect";
 import { inngest } from "@/lib/inngest";
 import { prisma } from "@/lib/prisma";
@@ -22,10 +23,33 @@ export const deleteOrganization = async (organizationId: string) => {
       return toActionState("ERROR", "Not a member of this organization");
     }
 
-    await inngest.send({
-      name: appConfig.events.names.organizationDeleted,
-      data: { organizationId },
+    const ticketsIds = await prisma.ticket.findMany({
+      where: { organizationId },
+      select: { id: true },
     });
+
+    const attachments = await prisma.attachment.findMany({
+      where: { ticketId: { in: ticketsIds.map((t) => t.id) } },
+      include: { ticket: true },
+    });
+
+    if (attachments && attachments.length) {
+      const attachmentsKeys = {
+        Objects: attachments.map((attachment) => ({
+          Key: generateS3Key({
+            organizationId: organizationId,
+            ticketId: attachment.ticket.id,
+            fileName: attachment.name,
+            attachmentId: attachment.id,
+          }),
+        })),
+      };
+
+      await inngest.send({
+        name: appConfig.events.names.organizationDeleted,
+        data: { organizationId, attachmentsKeys },
+      });
+    }
 
     await prisma.organization.delete({
       where: { id: organizationId },
--- src/features/organization/events/event-organization-deleted.ts
+++ src/features/organization/events/event-organization-deleted.ts
@@ -1,15 +1,16 @@
-import { NonRetriableError } from "inngest";
-
-import { generateS3Key } from "@/features/attachments/utils/generate-s3-key";
 import { s3 } from "@/lib/aws";
 import { inngest } from "@/lib/inngest";
-import { prisma } from "@/lib/prisma";
 import { appConfig } from "@/utils/app-config";
 import { DeleteObjectsCommand } from "@aws-sdk/client-s3";
 
 export type OrganizationDeletedEventArgs = {
   data: {
     organizationId: string;
+    attachmentsKeys: {
+      Objects: {
+        Key: string;
+      }[];
+    };
   };
 };
 
@@ -19,53 +20,23 @@ export const organizationDeletedCleanupEvent = inngest.createFunction(
     event: appConfig.events.names.organizationDeleted,
   },
   async ({ event, step }) => {
-    const { organizationId } = event.data;
+    const { attachmentsKeys } = event.data;
 
-    const tickets = await step.run(
-      "get-tickets",
-      async () =>
-        await prisma.ticket.findMany({
-          where: { organizationId },
-          select: { id: true },
-        }),
-    );
-
-    if (!tickets || !tickets.length) {
-      throw new NonRetriableError("No tickets have been found");
-    }
-
-    const attachmentsKeys = await step.run("get-attachments-keys", async () => {
-      const attachments = await prisma.attachment.findMany({
-        where: { ticketId: { in: tickets.map((t) => t.id) } },
-        include: { ticket: true },
+    await step
+      .run(
+        "delete-attachments-s3",
+        async () =>
+          await s3.send(
+            new DeleteObjectsCommand({
+              Bucket: process.env.AWS_BUCKET_NAME,
+              Delete: attachmentsKeys,
+            }),
+          ),
+      )
+      .catch((error) => {
+        console.log(error);
+        return { event, body: false };
       });
-
-      if (!attachments || !attachments.length) {
-        throw new NonRetriableError("No attachments have been found");
-      }
-
-      return {
-        Objects: attachments.map((attachment) => ({
-          Key: generateS3Key({
-            organizationId: organizationId,
-            ticketId: attachment.ticket.id,
-            fileName: attachment.name,
-            attachmentId: attachment.id,
-          }),
-        })),
-      };
-    });
-
-    await step.run(
-      "delete-attachments-s3",
-      async () =>
-        await s3.send(
-          new DeleteObjectsCommand({
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Delete: attachmentsKeys,
-          }),
-        ),
-    );
 
     return { event, body: true };
   },

f677d34eef32abbb9090d2d4e5c3d5d7f1884c12
AGomezSittima <agomezsittimadeveloper@gmail.com>
2025-09-11 02:25:13 +0200

    protected route

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,6 +1,5 @@
 import { getTickets } from "@/features/ticket/queries/get-tickets";
 import { searchParamsCache } from "@/features/ticket/search-params";
-import { prisma } from "@/lib/prisma";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -15,25 +14,4 @@ export async function GET(request: Request) {
   );
 
   return Response.json({ list, metadata });
-}
-
-export async function DELETE(
-  { headers }: Request,
-  { params }: { params: Promise<{ ticketId: string }> },
-) {
-  const { ticketId } = await params;
-
-  const ticket = await prisma.ticket.findUnique({ where: { id: ticketId } });
-
-  if (!ticket) {
-    return Response.json({ error: "Ticket not found" }, { status: 404 });
-  }
-
-  await prisma.ticket.delete({
-    where: {
-      id: ticketId,
-    },
-  });
-
-  return Response.json({ ticketId });
 }
